b2e8686efcee0ea6a5efd8787d8bc0f768dce30f
Laura Carazo <laura@self-couture.com>
2020-12-11 11:25:10 +0100

    Affichage du loading... en attendant la response

--- src/components/test-axios/TestAxios.jsx
+++ src/components/test-axios/TestAxios.jsx
@@ -7,20 +7,21 @@ class TestAxios extends React.Component{
         super(props);
         this.state = {
             isLoaded : false,
-            joke: ""
+            
         }
     }
 
     componentDidMount() {
         Axios.get("http://api.icndb.com/jokes/random")
             .then((response) => {
-                this.setState({joke : response.data.value.joke });
+                this.setState({value : response.data.value, isLoaded:true });
             });
     }
 
     render (){
+        if (!this.state.value) return <h4>Loading ...</h4>
         return (
-            <h1>{ this.state.joke}</h1>
+            <h1>{ this.state.value.joke}</h1>
         )
     }
 
