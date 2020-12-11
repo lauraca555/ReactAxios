593ad8389c2e0d6504b6751a95a285d47dc727df
Laura Carazo <laura@self-couture.com>
2020-12-11 11:14:03 +0100

    recuperation d'une response

--- src/components/test-axios/TestAxios.jsx
+++ src/components/test-axios/TestAxios.jsx
@@ -6,17 +6,21 @@ class TestAxios extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            isLoaded : false
+            isLoaded : false,
+            joke: ""
         }
     }
 
     componentDidMount() {
-        Axios.get("http://api.icndb.com/jokes/random");
+        Axios.get("http://api.icndb.com/jokes/random")
+            .then((response) => {
+                this.setState({joke : response.data.value.joke });
+            });
     }
 
     render (){
         return (
-            <h1>Axios</h1>
+            <h1>{ this.state.joke}</h1>
         )
     }
 
