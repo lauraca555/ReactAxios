658585faf553c0055b8ce82531c9a4bead05158b
Laura Carazo <laura@self-couture.com>
2020-12-11 11:35:46 +0100

    post ou put

--- src/components/test-axios/TestAxios.jsx
+++ src/components/test-axios/TestAxios.jsx
@@ -12,14 +12,29 @@ class TestAxios extends React.Component{
     }
 
     componentDidMount() {
-        Axios.get("http://api.icndb.com/jokes/random")
+        /*Axios.get("http://api.icndb.com/jokes/random")
             .then((response) => {
                 this.setState({value : response.data.value, isLoaded:true });
             })
             .catch((error) => {
                 console.log(error);
-            })
-    }
+            })*/
+        
+            const user = {
+                name : 'toto'
+            }
+
+            Axios.post('https://jsonplaceholder.typicode.com/users', user)
+                    .then((response) => {
+                        console.log(response.data);
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    })   
+
+
+        }
+
 
     render (){
         if (!this.state.value) return <h4>Loading ...</h4>
